3319eaaf71d6fe938072bbeac1238eaf83ec57bf
Bahnasy2001 <hassanbahnasy872@gmail.com>
2024-10-15 14:07:33 +0300

    Update

--- terraform/virtual_machine.tf
+++ terraform/virtual_machine.tf
@@ -91,7 +91,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "24.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
   # Define the operating system disk
